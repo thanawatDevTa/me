fdcfe3abc0a4c2df8b5e5a85f0dfd2de38d841ad
thanawatDevTa <147481945+thanawatDevTa@users.noreply.github.com>
2023-12-09 22:08:52 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+// import ReactGA from "react-ga4";
+
+import Homepage from "./pages/homepage";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Articles from "./pages/articles";
+import Contact from "./pages/contact";
+
+// import { TRACKING_ID } from "./data/tracking";
+import "./app.css";
+
+function App() {
+	// useEffect(() => {
+	// 	if (TRACKING_ID !== "") {
+	// 		ReactGA.initialize(TRACKING_ID);
+	// 	}
+	// }, []);
+
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/articles" element={<Articles />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Portfolio",
+		name: "Thanawat K.",
+		email: "Thanawat152001@gmail,com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://www.instagram.com/thanawat_1501/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/vasgoo/",
+	},
+
+	homepage: {
+		title: "Full-stack web .",
+		description:
+			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Thanawat Kannok. I live in Thailand, where I design the future.",
+		description:
+			" My name is Thanawat Kannok. You can call me Nueng. I'm a fresh graduate at Rajamangala University of Technology isan in Computert Engineering. I have experience in making a dormitory management system.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Dormitory system",
+			description:
+				"Prepare a dormitory system project with php language.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/php/php.png",
+			linkText: "View Project",
+			link: "https://rmuti.ac.th/user/tipa/dormitory/login.php",
+		},
+
+		{
+			title: "Akira-iot",
+			description:
+				"It is a system for reporting the status of solar cell towers, developed with next js type script language.",
+			logo: "https://gorzelinski.com/static/1db41e3ecd311724a15306b270d99dd9/6e87d/next-js-logo.png",
+			linkText: "View Project",
+			link: "http://akira-iot.com/index",
+		},
+
+		// {
+		// 	title: "Project 3",
+		// 	description:
+		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+
+		// {
+		// 	title: "Project 4",
+		// 	description:
+		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+
+		// {
+		// 	title: "Project 5",
+		// 	description:
+		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+	],
+};
+
+export default INFO;
